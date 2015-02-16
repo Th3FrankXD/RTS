d234481497368d419277f37243a9158956d40014
unknown <fdeweers@gmail.com>
2015-02-16 20:27:53 +0100

    added fps cap

--- First SFML Game/src/First SFML Game/main.cpp
+++ First SFML Game/src/First SFML Game/main.cpp
@@ -1,9 +1,13 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 #include <string>
+#include <windows.h>
 #include "gameObjects.h"
 
+int fpsCap = 60;
+
 Elite enemy;
+sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
 
 sf::Sprite* drawSprite(const sf::Texture* texture, float xLoc, float yLoc, float rotation)
 {
@@ -20,7 +24,6 @@ sf::Sprite* drawSprite(const sf::Texture* texture, float xLoc, float yLoc, float
 	return sprite;
 }
 
-
 class World
 {
 public:
@@ -36,6 +39,34 @@ World::World()
 //WorldLoop
 void World::update()
 {
+	if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
+	{
+		enemy.target = sf::Mouse::getPosition(window);
+	}
+
+	if (enemy.xLoc - enemy.target.x < -(enemy.speed / 5) || enemy.xLoc - enemy.target.x > (enemy.speed / 5))
+	{
+		if (enemy.xLoc < enemy.target.x)
+		{
+			enemy.xLoc += enemy.speed;
+		}
+		else
+		{
+			enemy.xLoc -= enemy.speed;
+		}
+	}
+
+	if (enemy.yLoc - enemy.target.y < -(enemy.speed / 5) || enemy.yLoc - enemy.target.y > (enemy.speed / 5))
+	{
+		if (enemy.yLoc < enemy.target.y)
+		{
+			enemy.yLoc += enemy.speed;
+		}
+		else
+		{
+			enemy.yLoc -= enemy.speed;
+		}
+	}
 }
 
 
@@ -44,13 +75,10 @@ class Render
 public:
 	Render();
 	void update(World& world);
-
-private:
-	sf::RenderWindow window;
 };
 
 //INIT
-Render::Render() : window(sf::VideoMode(800, 600), "My window")
+Render::Render()
 {
 }
 
@@ -69,34 +97,10 @@ void Render::update(World& world)
 	// clear the window with black color
 	window.clear(sf::Color::Black);
 	
-	
 	// draw everything here...
 	sf::Sprite* sprite = drawSprite(enemy.texture, enemy.xLoc, enemy.yLoc, enemy.rotation);
 	window.draw(*sprite);
 	delete sprite;
-
-	if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
-	{
-		enemy.target = sf::Mouse::getPosition(window);
-	}
-
-	if (enemy.xLoc < enemy.target.x)
-	{
-		enemy.xLoc += enemy.speed;
-	}
-	else
-	{
-		enemy.xLoc -= enemy.speed;
-	}
-
-	if (enemy.yLoc < enemy.target.y)
-	{
-		enemy.yLoc += enemy.speed;
-	}
-	else
-	{
-		enemy.yLoc -= enemy.speed;
-	}
 
 	// end the current frame
 	window.display();
@@ -108,9 +112,25 @@ int main()
 	World world;
 	Render renderer;
 	
+	std::clock_t start;
+	std::clock_t end;
+	double duration;
+	double sleepTime;
+
 	while (true)
 	{
+		start = std::clock();
+
 		world.update();
 		renderer.update(world);
+
+		end = std::clock();
+
+		duration = (end - start) / (CLOCKS_PER_SEC / 1000);
+		sleepTime = 1000 / fpsCap - duration;
+		if (sleepTime > 0)
+		{
+			Sleep(sleepTime);
+		}
 	}
 }
