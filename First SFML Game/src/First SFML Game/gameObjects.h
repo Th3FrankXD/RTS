757232e68df3807cde95de3cd7b1b9e8f070aa8e
unknown <fdeweers@gmail.com>
2015-03-10 17:31:15 +0100

    no message

--- First SFML Game/src/First SFML Game/gameObjects.h
+++ First SFML Game/src/First SFML Game/gameObjects.h
@@ -29,6 +29,13 @@ class Spartan :public Unit
 {
 public:
 	Spartan();
+	//sf::Texture* texture = textures.spartan;
+	sf::Vector2f location;
+	sf::Color color = sf::Color(255, 255, 255);
+	float rotation = 0.0;
+	float speed = 3.0;
+	bool selected;
+	sf::Vector2i target;
 };
 
 
